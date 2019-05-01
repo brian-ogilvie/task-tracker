764811ec116b4ca926cfe8f4d17b69e247a80ac8
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-04-30 21:49:28 -0400

    added Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Board from '../Board/Board'
+import Header from '../Header/Header'
 
 class App extends PureComponent {
   render() {
     return (
       <div className="App">
+        <Header />
         <Board />
       </div>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Header__title">Task Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
